be5836644f0db0c6ba1e57e69adc29cc79121772
ludotec <claudiofuentez6@gmail.com>
2020-12-30 10:25:52 -0300

    updates to deploy to heroku3

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/steweb'));
+app.use(express.static('./dist/new-steweb'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/steweb/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/new-steweb/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
